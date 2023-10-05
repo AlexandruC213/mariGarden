5adee249d682311010d60827452ccfe161deed7e
Chis Alexandru <alexandru_chis213@yahoo.com>
2023-10-06 00:11:42 +0300

    add didst

---
+++ js/chunk-2d22454f.726e5d45.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22454f"],{e04b:function(t,n,e){"use strict";e.r(n);var o=function(){var t=this,n=t._self._c;return n("div",{staticClass:"notFound-container"},[n("h1",[t._v("Network Issue")]),n("h3",[t._v("Come back later!")]),n("router-link",{attrs:{to:{name:"home"}}},[t._v("Go back to Home")])],1)},a=[],s=e("2877"),r={},u=Object(s["a"])(r,o,a,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d22454f.726e5d45.js.map
--- src/views/errors/NetworkIssue.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="notFound-container">
-    <h1>Network Issue</h1>
-    <h3>Come back later!</h3>
-    <router-link :to="{ name: 'home' }">Go back to Home</router-link>
-  </div>
-</template>
